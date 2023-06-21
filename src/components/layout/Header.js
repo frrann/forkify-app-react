1b9830000a2725d8ef32221297a035eb000e0b84
frrann <ada.fran92@gmail.com>
2023-06-21 14:39:07 +0300

    Added header component & styles

---
+++ src/components/layout/Header.js
@@ -0,0 +1,54 @@
+import classes from "./Header.module.scss";
+import Logo from "../../assets/images/logo.png";
+import Icons from "../../assets/images/icons.svg";
+
+const Header = () => {
+  const searchBar = <p>Search recipe!</p>;
+
+  const navContent = (
+    <nav className={classes.nav}>
+      <ul className={classes.nav__list}>
+        <li className={classes.nav__item}>
+          <button
+            className={`${classes.nav__btn} ${classes["nav__btn--add-recipe"]}`}
+          >
+            <svg className={classes.nav__icon}>
+              <use href={`${Icons}#icon-edit`}></use>
+            </svg>
+            <span>Add recipe</span>
+          </button>
+        </li>
+        <li className={classes.nav__item}>
+          <button
+            className={`${classes.nav__btn} ${classes["nav__btn--bookmarks"]}`}
+          >
+            <svg className={classes.nav__icon}>
+              <use href={`${Icons}#icon-bookmark`}></use>
+            </svg>
+            <span>Bookmarks</span>
+          </button>
+          <div className={classes.bookmarks}>
+            <ul className={classes.bookmarks__list}>
+              <div className={classes.message}>
+                <svg>
+                  <use href={`${Icons}#icon-smile`}></use>
+                </svg>
+              </div>
+              <p>No bookmarks yet. Find a nice recipe and bookmark it :)</p>
+            </ul>
+          </div>
+        </li>
+      </ul>
+    </nav>
+  );
+
+  return (
+    <header className={classes.header}>
+      <img src={Logo} alt="Logo" className={classes.header__logo} />
+      {searchBar}
+      {navContent}
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/data/recipes-data.js
@@ -0,0 +1,28 @@
+export const recipes = [
+  {
+    publisher: "Simply Recipes",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/howtogrillpizzad300x20086a60e1b.jpg",
+    title: "How to Grill Pizza",
+    id: "5ed6604591c37cdc054bcb37",
+  },
+  {
+    publisher: "All Recipes",
+    image_url: "http://forkify-api.herokuapp.com/images/391236ba85.jpg",
+    title: "Veggie Pizza",
+    id: "5ed6604591c37cdc054bca5d",
+  },
+  {
+    publisher: "Closet Cooking",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/Pizza2BDip2B12B500c4c0a26c.jpg",
+    title: "Pizza Dip",
+    id: "5ed6604591c37cdc054bcac4",
+  },
+  {
+    publisher: "BBC Good Food",
+    image_url: "http://forkify-api.herokuapp.com/images/1649634_MEDIUMd3fc.jpg",
+    title: "Pitta pizzas",
+    id: "5ed6604591c37cdc054bc990",
+  },
+];
